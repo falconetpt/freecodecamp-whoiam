d97c153e068091faa2bbf6e0b98a7b015e391100
Ricardo Gomes <ipsiart@gmail.com>
2020-09-29 15:57:50 +0100

    init

---
+++ src/main/kotlin/com/freecodecamp/headers/api/Reply.kt
@@ -0,0 +1,3 @@
+package com.freecodecamp.headers.api
+
+data class Reply(val ipaddress: String, val language: String, val software: String)
---
+++ src/main/kotlin/com/freecodecamp/headers/api/WhoAmIController.kt
@@ -0,0 +1,28 @@
+package com.freecodecamp.headers.api
+
+import org.springframework.http.HttpHeaders
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestHeader
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/whoami")
+class WhoAmIController {
+    @GetMapping
+    fun get(@RequestHeader headers: HttpHeaders): ResponseEntity<Reply> {
+        println(headers)
+        val software = headers.getValuesAsList("user-agent").fold("") {
+            acc, s -> acc + s
+        }
+        val language = headers.getValuesAsList("accept-language").fold("") {
+            acc, s -> acc + s
+        }
+        val host = headers.getValuesAsList("host").fold("") {
+            acc, s -> acc + s
+        }
+
+        return ResponseEntity.ok(Reply(host, language, software))
+    }
+}
