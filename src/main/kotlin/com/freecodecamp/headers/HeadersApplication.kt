7e835ae79ea608db86d60de38229a362a07e8a0e
Ricardo Gomes <ipsiart@gmail.com>
2020-09-29 15:31:32 +0100

    init

---
+++ src/main/kotlin/com/freecodecamp/headers/HeadersApplication.kt
@@ -0,0 +1,11 @@
+package com.freecodecamp.headers
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HeadersApplication
+
+fun main(args: Array<String>) {
+    runApplication<HeadersApplication>(*args)
+}
---
+++ src/test/kotlin/com/freecodecamp/headers/HeadersApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.freecodecamp.headers
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HeadersApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
